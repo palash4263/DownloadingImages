049cd7d3fd2edc95cba9e94942b2e0dc81b3a2f1
Palash Mishra <palashmishra47@gmai.com>
2021-06-19 00:41:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/downloadingimages/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.downloadingimages;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    public void downloadImage(View view){
+        ImageDownloader task = new ImageDownloader();
+        Bitmap myImage;
+        try {
+            myImage = task.execute("https://upload.wikimedia.org/wikipedia/en/8/82/DJ_Khaled_-_Khaled_Khaled.png").get();
+
+            imageView.setImageBitmap(myImage);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+
+    }
+
+    public class ImageDownloader extends AsyncTask<String,Void, Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL url  = new URL(urls[0]);
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream in = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(in);
+
+                return myBitmap;
+            } catch (Exception e) {
+                e.printStackTrace();
+               return null;
+            }
+        }
+    }
+
+
+}
